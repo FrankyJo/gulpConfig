87ae8db4715f7e74405fa9e9488d1a7fd87249c4
DenysPopov <denispo@wizardsdev.com>
2020-02-12 16:39:07 +0200

    add in gulp config gulp-minify and browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,47 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var browserSync = require('browser-sync');
 
 sass.compiler = require('node-sass');
 
+var reload = browserSync.reload;
 
-gulp.task('hello', function() {
-    console.log('Hello Zell');
-});
+var paths = {
+    html:['app/index.html'],
+    css:['app/scss/**/*.scss']
+};
+
 
 gulp.task('sass', function () {
-    return gulp.src('app/scss/**/*.scss')
+    return gulp.src(paths.css)
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(minifyCss())
+        .pipe(gulp.dest('dist/css'))
+        .pipe(reload({stream:true}));
 });
+
+gulp.task('html',function(){
+    return gulp.src(paths.html)
+        .pipe(gulp.dest('dist'))
+        .pipe(reload({stream:true}));
+});
+
+gulp.task('browserSync', function() {
+    browserSync({
+        server: {
+            baseDir: "./dist"
+        },
+        port: 8080,
+        open: true,
+        notify: false
+    });
+
+});
+
+gulp.task('watcher',function(){
+    gulp.watch(paths.css, gulp.series('sass'));
+    gulp.watch(paths.html, gulp.series('html'));
+});
+
+gulp.task('default', gulp.series(gulp.parallel('watcher', 'browserSync')));
