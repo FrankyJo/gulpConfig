987cdf767fe963faaadc530ce1866af315f83696
DenysPopov <denispo@wizardsdev.com>
2020-02-13 12:15:15 +0200

    add autoprefixer and merge media queries

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,16 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var browserSync = require('browser-sync');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const minifyCss = require('gulp-minify-css');
+const browserSync = require('browser-sync');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const mmq = require('gulp-merge-media-queries');
 
 sass.compiler = require('node-sass');
 
-var reload = browserSync.reload;
+const reload = browserSync.reload;
 
-var paths = {
+const paths = {
     html:['app/index.html'],
     css:['app/scss/**/*.scss']
 };
@@ -15,8 +18,14 @@ var paths = {
 
 gulp.task('sass', function () {
     return gulp.src(paths.css)
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer({
+            cascade: false
+        }))
         .pipe(sass().on('error', sass.logError))
+        .pipe(mmq())
         .pipe(minifyCss())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('dist/css'))
         .pipe(reload({stream:true}));
 });
